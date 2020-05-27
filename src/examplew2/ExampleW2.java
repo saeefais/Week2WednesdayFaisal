5eac381c15b69b7c0666faadbff510bcdb8f3878
saeefais <saeefais@sheridancollege.ca>
2020-05-27 18:04:07 -0400

    Test1

---
+++ src/examplew2/ExampleW2.java
@@ -0,0 +1,17 @@
+package examplew2;
+
+/**
+ *This class +++insert description here+++
+ *
+ * @author Faisal Saeed
+ */
+public class ExampleW2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Push to repository");
+    }
+
+}
